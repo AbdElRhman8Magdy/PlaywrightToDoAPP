4804654d7b9a1f3b0530f441abd870d305329983
AbdElRhman8Magdy <abdelrhman8magdy@gmail.com>
2024-08-13 11:10:04 +0300

    💡IMPROVE: eddit config timeOut, Add GitHub actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,11 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+
+  timeout:  100 * 100, 
+  expect:{
+    timeout:   60 * 100
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
